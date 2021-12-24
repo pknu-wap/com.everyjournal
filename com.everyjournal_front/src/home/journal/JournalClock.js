20246cbbde62c8a230c7f988e672f8b07581e9f0
JH <leaderofst10@gmail.com>
2021-12-24 23:46:08 +0900

    Front journal timer done

--- com.everyjournal_front/src/home/journal/JournalClock.js
+++ com.everyjournal_front/src/home/journal/JournalClock.js
@@ -1,4 +1,5 @@
 
+import axios from 'axios';
 import React, { Component } from 'react';
 
 class JournalClock extends Component {
@@ -11,15 +12,25 @@ class JournalClock extends Component {
     }
 
     render() {
+        let curTimeStr = '';
+        if(this.state.curTime>=60) {
+            curTimeStr += parseInt(this.state.curTime/60) + ':';
+            if(this.state.curTime%60 < 10) curTimeStr += '0' + this.state.curTime%60;
+            else curTimeStr += this.state.curTime%60;
+        } else {
+            curTimeStr += '00:';
+            if(this.state.curTime%60 < 10) curTimeStr += '0' + this.state.curTime%60;
+            else curTimeStr += this.state.curTime%60;
+        }
+
     return(
         <div>
             목표 수행 시간{this.props.targetTime}
-            현재 수행 시간:{this.state.curTime}
+            현재 수행 시간{curTimeStr}
             <input type='button' value={'Start!'} 
             onClick={(e)=>{
                 if(this.timer===null) {
                     this.timer = setInterval(()=>{
-                        console.log('ct:', this.state.curTime);
                         this.setState({curTime: this.state.curTime+1});
                     },1000);
                 }
@@ -28,6 +39,20 @@ class JournalClock extends Component {
             onClick={(e)=>{
                 clearInterval(this.timer);
                 this.timer = null;
+                axios.put(`/api/journal/target/${this.props.id}/cur_time/set/${this.state.curTime}`)
+                .then((res)=>{
+                    if(res.status===200) { // 작성 성공
+                        console.log(res.status);
+                        this.props.getJournals();
+                    } else if(res.status===412) { // 작성 실패
+                        console.log(res.status,res.data);
+                        this.props.onError();
+                    }
+                })
+                .catch((err)=>{
+                    console.log(err);
+                    //this.props.onError(); // #test
+                });
             }}/>
         </div>
     );
